78014b94dd5be35e9b32fc491952481d6e8ab46c
Zachi Greenberger <zachi.greenberger@gmail.com>
2017-11-30 05:16:50 +0200

    js into objects refactoring

--- reports/index.js
+++ reports/index.js
@@ -1,67 +1,113 @@
-﻿
-var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+﻿var measures = (function () {
+  var margin = { top: 20, right: 20, bottom: 30, left: 50 },
     outerWidth = 1000,
     outerHeight = 500,
     width = outerWidth - margin.left - margin.right,
     height = outerHeight - margin.top - margin.bottom;
 
-var svg = d3.select(".report").append("svg").attr("width", outerWidth).attr("height", outerHeight).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  return {
+    margin: margin,
+    outerWidth: outerWidth,
+    outerHeight: outerHeight,
+    width: width,
+    height: height
+  }
 
-var yAxisScale = d3.scaleLinear().domain([100, 0]).range([0, height]);
-var yAxis = d3.axisLeft(yAxisScale);
-var yAxisGroup = svg.append("g").classed("y axis", true).call(yAxis);
 
-var xAxisScale = null;
+})();
 
-var tip = d3.select(".report").append("div").attr("class", "d3-tip").style("opacity", 0);
+var axes = (function () {
 
-d3.json("data.json", function (data) {
+
+  function init(data, svg) {
+
+    axes.yAxisScale = d3.scaleLinear().domain([100, 0]).range([0, measures.height]);
+    var yAxis = d3.axisLeft(axes.yAxisScale);
+    yAxisGroup = svg.append("g").classed("y axis", true).call(yAxis);
+
+    axes.xAxisScale = d3.scaleLinear().domain([0, data.folder.questionnaires.length]).range([0, measures.width]);
+    var xAxis = d3.axisBottom(axes.xAxisScale).tickSize(-measures.height).tickFormat(function (d) { return d == 0 ? '' : data.folder.questionnaires[d - 1].name; });
+    svg.append("g").classed("x axis", true).attr("transform", "translate(0," + measures.height + ")").call(xAxis);
+  }
+
+
+  return {
+    yAxisScale: null,
+    xAxisScale: null,
+    init: init
+  }
+
+})();
+
+var abilities = (function () {
+
+  function transform(ability) {
+    return "translate(" + axes.xAxisScale(ability["questionnaire-order"]) + "," + axes.yAxisScale(ability["value"]) + ")";
+
+  }
+  function init(data, svg) {
+
+    var abilityTip = d3.select(".report").append("div").attr("class", "ability-tip").style("opacity", 0);
+
+    var objects = svg.append("svg").classed("objects", true).attr("width", measures.width).attr("height", measures.height);
+    objects.selectAll(".dot")
+      .data(data.folder.abilities)
+      .enter().append("circle")
+      .classed("dot", true)
+      .attr("r", "1")
+      .attr("transform", transform)
+      .style("fill", function (ability) { return ability.finished ? "#00CF00" : "#32BBFF"; })
+      .on("mouseover", function (ability) {
+        abilityTip.html(function () {
+          var result = "";
+          for (var i = 0; i < ability.students.length; i++) {
+            result += ability.students[i].name + "<br>";
+          }
+          return result;
+        });
+
+        var top = axes.yAxisScale(ability["value"]) + measures.margin.top - (ability.students.length * 10) - (abilityTip.node().getBoundingClientRect().height);
+        var left = axes.xAxisScale(ability["questionnaire-order"]) + measures.margin.left - (abilityTip.node().getBoundingClientRect().width / 2);
+        abilityTip.style("transform", "translate(" + left + "px ," + top + "px )")
+          .style("opacity", .9);
+
+
+      })
+      .on("mouseout", function (d) {
+        abilityTip.style("opacity", 0);
+      })
+      .transition().attr("r", function (ability) { return ability.students.length * 10; }).duration(1000);
+    
+  }
+
+  return {
+    init: init
+  }
+
+})();
+
+
+(function () {
+
+  var svg = d3.select(".report").append("svg").attr("width", measures.outerWidth).attr("height", measures.outerHeight).append("g").attr("transform", "translate(" + measures.margin.left + "," + measures.margin.top + ")");
+  
+
+  d3.json("data.json", function (data) {
 
     data = loadDataFromQueryIfNecessary(data);
-
-    xAxisScale = d3.scaleLinear().domain([0, data.folder.questionnaires.length]).range([0, width]);
+    axes.init(data, svg);
+    abilities.init(data,svg);
     
-    var xAxis = d3.axisBottom(xAxisScale).tickSize(-height).tickFormat(function (d) { return d == 0 ? '' : data.folder.questionnaires[d - 1].name; });
-    var xAxisGroup = svg.append("g").classed("x axis", true).attr("transform", "translate(0," + height + ")").call(xAxis);
-    
-    var objects = svg.append("svg").classed("objects", true).attr("width", width).attr("height", height);
-    objects.selectAll(".dot")
-        .data(data.folder.abilities)
-        .enter().append("circle")
-        .classed("dot", true)
-        .attr("r", "1")
-        .attr("transform", transform)
-        .style("fill", function (ability) { return ability.finished ? "#00CF00" : "#32BBFF"; })
-        .on("mouseover", function (ability) {
-            tip.html(function () {
-                var result = "";
-                for (var i = 0; i < ability.students.length; i++) {
-                    result += ability.students[i].name + "<br>";
-                }
-                return result;
-            });
 
-            var top = yAxisScale(ability["value"]) + margin.top - (ability.students.length * 10) - (tip.node().getBoundingClientRect().height);
-            var left = xAxisScale(ability["questionnaire-order"]) + margin.left - (tip.node().getBoundingClientRect().width / 2);
-            tip.style("transform", "translate(" + left + "px ," + top + "px )")
-                .style("opacity", .9);
+  });
 
 
-        })
-        .on("mouseout", function (d) {
-            tip.style("opacity", 0);
-        })    
-        .transition().attr("r", function (ability) { return ability.students.length * 10; }).duration(1000);
-
-});
-
-function transform(ability) {
-    return "translate(" + xAxisScale(ability["questionnaire-order"]) + "," + yAxisScale(ability["value"]) + ")";
-
-}
-
-function loadDataFromQueryIfNecessary(data) {
+  function loadDataFromQueryIfNecessary(data) {
     if (decodeURIComponent(document.location.search))
-        return JSON.parse(decodeURIComponent(document.location.search.substring(1)));
+      return JSON.parse(decodeURIComponent(document.location.search.substring(1)));
     return data;
-}
+  }
+
+
+})()
+
